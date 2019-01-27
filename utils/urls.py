b41f54a98676121a8dd99312c39ede984f70bddf
lenconda <1053464288@qq.com>
2019-01-27 17:27:43 +0800

    👍 feat(utils): finish utils.urls

--- utils/urls.py
+++ utils/urls.py
@@ -3,6 +3,7 @@
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from urllib.parse import urlparse
 from urllib.error import URLError, HTTPError
 import re
 
@@ -17,7 +18,23 @@ def check_if_url(url):
     url_test = re.compile(url_test_regexp)
     return url_test.match(url)
 
-is_url = check_if_url('https://www.baidu.com')
-print(is_url)
+def inner_url(current_url, site_url):
+    current_host = urlparse(current_url).hostname
+    site_host = urlparse(site_url).hostname
+    if current_host:
+        if current_host == site_host:
+            return True
+        else:
+            return False
+    return True
+
+def prefix_url(current_url, site_url_prefix):
+    current_url_parse = urlparse(current_url)
+    if current_url_parse.hostname == None \
+            and current_url_parse.path[0] == '/':
+        return site_url_prefix + current_url_parse.path
+    else:
+        return site_url_prefix + '/' + current_url_parse.path
+
 
 # if __name__ == '__main__':
