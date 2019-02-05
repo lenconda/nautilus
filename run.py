b5b719a6df4fbdf2f9eef49531d298d1033b854e
lenconda <1053464288@qq.com>
2019-02-05 16:47:47 +0800

    🎨 an elegant exit code

--- run.py
+++ run.py
@@ -3,7 +3,13 @@
 
 from linkfolio.linkfolio import LinkFolio
 from utils.config import SEED_URL, MAX_DEPTH
+from utils.logger import get_logger
 
 if __name__ == '__main__':
-    app = LinkFolio(url = SEED_URL, max_depth = MAX_DEPTH)
-    app.run()
+    logger = get_logger('main')
+    try:
+        app = LinkFolio(url = SEED_URL, max_depth = MAX_DEPTH)
+        app.run()
+    except KeyboardInterrupt as e:
+        logger.info('Exit after KeyboardInterrupt...')
+        exit(0)
