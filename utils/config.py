e22ac9344963dc4b986637b608629a04182b206b
lenconda <1053464288@qq.com>
2019-02-05 16:26:51 +0800

    🎨 prettify config keys

--- utils/config.py
+++ utils/config.py
@@ -9,15 +9,15 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 # application configs
-MAX_DEPTH = config['BASIC']['MaxDepth'] or 16
+MAX_DEPTH = config['basic']['max_depth'] or 16
 SEED_URL = envs.get('LINKFOLIO_SEED_URL') \
-           or config['BASIC']['SeedUrl'] \
+           or config['basic']['seed_url'] \
            or 'http://www.example.com'
 
 # database configs
-DB_HOST = config['DATABASE']['Host'] or '127.0.0.1'
-DB_PORT = int(config['DATABASE']['Port']) or 3306
-DB_USER = config['DATABASE']['User'] or 'root'
-DB_PASSWORD = config['DATABASE']['Password'] or '123'
-DB_DATABASE = config['DATABASE']['Database'] or 'linkfolio'
-DB_CHARSET = config['DATABASE']['Charset'] or 'utf8mb4'
+DB_HOST = config['database']['host'] or '127.0.0.1'
+DB_PORT = int(config['database']['port']) or 3306
+DB_USER = config['database']['user'] or 'root'
+DB_PASSWORD = config['database']['password'] or '123'
+DB_DATABASE = config['database']['database'] or 'linkfolio'
+DB_CHARSET = config['database']['charset'] or 'utf8mb4'
