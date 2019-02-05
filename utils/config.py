89951987d8bc2b7770174e79eba70866442450c2
lenconda <1053464288@qq.com>
2019-02-05 16:55:11 +0800

    🐛 add docker env vars

--- utils/config.py
+++ utils/config.py
@@ -10,14 +10,21 @@ config.read('config.ini')
 
 # application configs
 MAX_DEPTH = config['basic']['max_depth'] or 16
-SEED_URL = envs.get('LINKFOLIO_SEED_URL') \
+SEED_URL = envs.get('SEED_URL') \
            or config['basic']['seed_url'] \
            or 'http://www.example.com'
 
 # database configs
-DB_HOST = config['database']['host'] or '127.0.0.1'
-DB_PORT = int(config['database']['port']) or 3306
-DB_USER = config['database']['user'] or 'root'
-DB_PASSWORD = config['database']['password'] or '123'
-DB_DATABASE = config['database']['database'] or 'linkfolio'
-DB_CHARSET = config['database']['charset'] or 'utf8mb4'
+DB_HOST = envs.get('DB_HOST') \
+          or config['database']['host'] \
+          or '127.0.0.1'
+DB_PORT = int(envs.get('DB_PORT')) \
+          or int(config['database']['port']) or 3306
+DB_USER = envs.get('DB_USER') \
+          or config['database']['user'] or 'root'
+DB_PASSWORD = envs.get('DB_PASSWORD') \
+              or config['database']['password'] or '123'
+DB_DATABASE = envs.get('DB_DATABASE') or \
+              config['database']['database'] or 'linkfolio'
+DB_CHARSET = envs.get('DB_CHARSET') \
+             or config['database']['charset'] or 'utf8mb4'
